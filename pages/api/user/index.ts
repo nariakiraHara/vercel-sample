257b6b687f04c5116b3145f896ce9af11baf8c5e
nhara <nhara@yahoo-corp.jp>
2021-08-06 13:26:56 +0900

    user add

---
+++ pages/api/user/index.ts
@@ -0,0 +1,21 @@
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getLogger } from 'log4js'
+
+type Data = {
+  name: string
+}
+
+const logger = getLogger()
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  logger.level = process.env.LOGGER_OUTPUT_LEVEL ?? ''
+  console.log('hello world')
+  logger.debug('some debug messages')
+  logger.error('error:some debug messages')
+  res.status(200).json({ name: 'John Doe' })
+}
